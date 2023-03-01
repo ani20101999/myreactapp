50a6d434a931863b4fcdecb3ae606d42f8bde92f
ani20101999 <rishurout20@gmail.com>
2023-03-02 02:15:24 +0530

    myreactapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  let[width,setWidth] = useState(0);
+  let[color,setColor] = useState("#ffffff");
+
+ let widthChange = (e)=>{
+  setWidth(e.target.value)
+ }
+ let colorChange = (e)=>{
+  setColor(e.target.value)
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <form action="#" method="POST"> */}
+      <div>
+       width: <input type="number" onChange={widthChange} value={width}></input>
+      </div>
+      <div>
+        color: <input type="color" onChange={colorChange} value={color}></input>
+      </div>
+      {/* </form> */}
+      <div id="rectangle" style={{width:width + "px",backgroundColor:color}}></div>
     </div>
   );
 }
 
 export default App;
+
